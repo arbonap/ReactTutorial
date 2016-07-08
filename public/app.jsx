1b61c2a91f01033fac3a62db397ca15e858b741d
Patricia Arbona <arbonap@gmail.com>
2016-07-07 17:43:25 -0700

    lecture 11 of react tutorial

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,29 @@
+var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React'
+        };
+    },
+    render: function () {
+        var name = this.props.name;
+        var message = this.props.message;
+
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>{message + '!!'}</p>
+            </div>
+           );
+        // return React.createElement(
+        //     'h1',
+        //     null,
+        //     'Hello React.createElement')
+        }
+    });
+
+var firstName = 'Patricia';
+
 ReactDOM.render(
-            <h1>Hello React!</h1>,
+            <Greeter name={firstName} message="Message from prop!"/>,
             document.getElementById('app')
             );
