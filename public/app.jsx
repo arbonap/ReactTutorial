ad076b35d7e5fa0ac03ddb5a82e97991438bc302
Patricia Arbona <arbonap@gmail.com>
2016-07-25 19:00:17 -0700

    finish lecture 17

--- public/app.jsx
+++ public/app.jsx
@@ -1,103 +1,86 @@
-// states can be changed, and props can't
 var GreeterMessage = React.createClass({
     render: function () {
-        var name = this.props.name;
-        var message = this.props.message;
-// Greeter Message access 2 props that are passed to it: name, message
-        return (
-            <div>
-            // renders these props to the component
-                <h1>Some H1 {name} ! </h1>
-                <p>Some paragraph </p>
-            </div>
-            );
+      var name = this.props.name;
+      var message = this.props.message;
+
+      return (
+        <div>
+          <h1>Hello {name}!</h1>
+          <p>{message}</p>
+        </div>
+      );
     }
 });
-// GreeterForm is also a presentational component
-// GreeterForm does not maintain its own state, it simply takes some props, & when the form gets submitted it calls a function; it never cares if the state gets updated,
+
 var GreeterForm = React.createClass({
-    onFormSubmit: function (e) {
-        e.preventDefault();
+  onFormSubmit: function (e) {
+    e.preventDefault();
 
-        var updates = {};
-        var name = this.refs.name.value;
-        var message = this.refs.messsage.value;
+    var updates = {};
+    var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
-        if (name.length > 0) {
-            this.refs.name.value = '';
-            updates.name = name;
-            this.props.onNewName(name);
-        }
-
-        if (message.length > 0) {
-            this.refs.message.value ='';
-            updates.message = message;
-        }
-        this.props.onNewName(updates);
-    },
-    render: function () {
-        return (
-
-                <form onSubmit={this.onFormSubmit}>
-                <input type="text" ref="name" placeholder="Enter Name" />
-                <textarea rows="3" cols="15" ref="message" placeholder="Enter Message"/>
-                <button>Set Name</button>
-                </form>
-
-            );
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      updates.name = name;
     }
-});
 
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewData(updates);
+  },
+  render: function () {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div>
+          <input type="text" ref="name" placeholder="Enter name"/>
+        </div>
+        <div>
+          <textarea ref="message" placeholder="Enter message"></textarea>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
+});
 
 var Greeter = React.createClass({
-    getDefaultProps: function () {
-        return {
-            name: 'React',
-            message: 'This is the default message, yo'
-        };
-    },
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  getInitialState: function () {
+    return {
+        name: this.props.name,
+        message: this.props.message
+    };
+  },
+  handleNewData: function (updates) {
+    this.setState(updates);
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.state.message;
 
-// set its name state to the props value
-    getInitialState: function () {
-        return {
-            name : this.props.name
-            // message : this.props.message
-        };
-    },
-// callback
-// handleNewName function looks for any change in the state of the variable name & if the new names changes, THEN it re-renders its child components within Greeter
-    handleNewName: function (name) {
-        this.setState({
-            name: name
-
-        });
-    // handleNewMessage: function
-
-// sets state of the attributes that you provide
-//updates the state and rerenders the parts of the component that matter
-    },
-    render: function () {
-        // this.state.name sets the value
-        var name = this.state.name;
-        var message = this.props.message;
-
-        return (
-            <div>
-
-
-
-                <GreeterMessage name={name} message={message}/>
-                <GreeterForm onNewName={this.handleNewName}/>
-
-            </div>
-            );
-    }
+    return (
+      <div>
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewData={this.handleNewData}/>
+      </div>
+    );
+  }
 });
 
-var firstName = 'Patricia';
+var firstName = 'Andrew';
 
 ReactDOM.render(
-    <Greeter name ={firstName}/>,
-
-    document.getElementById('app')
-    );
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
